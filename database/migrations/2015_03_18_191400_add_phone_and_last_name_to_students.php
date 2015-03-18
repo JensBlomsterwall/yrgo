507b2671747e72c28c20fac8affa1cd5e07909da
Oskar Adin <oad.work@gmail.com>
2015-03-18 20:42:18 +0100

    Migrations för våra studenter

---
+++ database/migrations/2015_03_18_191400_add_phone_and_last_name_to_students.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPhoneAndLastNameToStudents extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('students', function(Blueprint $table)
+		{
+			$table->string('last_name', 50);
+            $table->integer('phone')->unsigned()->nullable();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('students', function(Blueprint $table)
+		{
+			$table->dropColumn('last_name');
+			$table->dropColumn('phone');
+		});
+	}
+
+}
